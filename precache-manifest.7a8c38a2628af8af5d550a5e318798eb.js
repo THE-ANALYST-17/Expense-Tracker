d6a78fcf2bd3c2b88eae93a99d617ca5ae9fcc42
Utts17 <utkarsh.singh@kayadev.com>
2021-10-26 13:05:50 +0530

    Updates

---
+++ precache-manifest.7a8c38a2628af8af5d550a5e318798eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5af7cb34a03f6fa03d3dddef7eb129",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "714a00161be9ae090066",
+    "url": "/Expense-Tracker/static/css/main.571e6405.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/Expense-Tracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Expense-Tracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714a00161be9ae090066",
+    "url": "/Expense-Tracker/static/js/main.dc7cda36.chunk.js"
+  },
+  {
+    "revision": "242ecf752266ee6ab889",
+    "url": "/Expense-Tracker/static/js/runtime-main.c08c79a1.js"
+  }
+]);
